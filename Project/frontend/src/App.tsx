2e30791b5e3b0aec8ca944fd095f5d713cd79da3
Hon Chi Kit <kinoras.macau@gmail.com>
2024-11-26 14:18:08 +0800

    proj: fix: update video feed src

--- Project/frontend/src/App.tsx
+++ Project/frontend/src/App.tsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
+// import axios from "axios";
 
 const App = () => {
-    const [count, setCount] = useState(0)
+    const [imageSrc, setImageSrc] = useState("");
 
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
-    )
-}
+        <div>
+            <h1>RealityCanvas</h1>
+            <img
+                src="http://127.0.0.1:5000/video_feed"
+                style={{ border: "1px solid black", maxWidth: "640px" }}
+            />
+        </div>
+    );
+};
 
-export default App
+export default App;
